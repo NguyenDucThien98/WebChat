0fe10727e3944f4c1537ecf38699dc2488e7dded
NguyenDucThien98 <39368428+NguyenDucThien98@users.noreply.github.com>
2019-03-04 23:51:22 +0700

    Revert "login -1"
    This reverts commit d8ce98a05a9a865e22fdf4fb22d35e7fb77073f3.

--- WebChat/Controllers/LoginController.cs
+++ WebChat/Controllers/LoginController.cs
@@ -1,26 +1,16 @@
-﻿using Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebChat.Models;
 
-namespace WebChat.Controllers {
-    public class LoginController : Controller {
+namespace WebChat.Controllers
+{
+    public class LoginController : Controller
+    {
         // GET: Login
-        [HttpGet]
-        public ActionResult Login() {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(LoginModel model) {
-            var result = new AccontModel().Login(model.Username,model.Encrypted_password);
-            if (result && ModelState.IsValid) {
-            } else {
-                ModelState.AddModelError("","Sai Username hoặc Password");
-            }
+        public ActionResult Login()
+        {
             return View();
         }
     }
